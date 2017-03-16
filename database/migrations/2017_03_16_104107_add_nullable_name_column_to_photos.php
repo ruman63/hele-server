b6df9c47e3d07a8b65c09ced84a90cb14d7e3bb3
Ruman Saleem <ruman63@gmail.com>
2017-03-16 16:13:08 +0530

    Fixed migrations

--- database/migrations/2017_03_16_104107_add_nullable_name_column_to_photos.php
+++ database/migrations/2017_03_16_104107_add_nullable_name_column_to_photos.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyPhotosNameNullable extends Migration
+class AddNullableNameColumnToPhotos extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class ModifyPhotosNameNullable extends Migration
      */
     public function up()
     {
-        Schema::table('photos', function(Blueprint $table){
-          $table->dropColumn('name');
-          $table->string('name')->nullable();
+        Schema::table('photos', function (Blueprint $table) {
+            $table->string('name')->nullable();
         });
     }
 
@@ -26,7 +25,8 @@ class ModifyPhotosNameNullable extends Migration
      */
     public function down()
     {
-      $table->dropColumn('name');
-      $table->string('name');
+        Schema::table('photos', function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
     }
 }
